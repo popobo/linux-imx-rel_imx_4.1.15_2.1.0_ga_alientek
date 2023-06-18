23c5d5351b5d5c4af986ffea8796d10e81f38a12
popobo <331361996@qq.com>
2023-06-18 16:45:22 +0800

    update imx6ull_alientek_emmc.sh

--- imx6ull_alientek_emmc.sh
+++ imx6ull_alientek_emmc.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf imx_alientek_emmc_defconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j4
+# make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+# make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf imx_alientek_emmc_defconfig
+# make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+# make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j4
+cp arch/arm/boot/zImage /home/bo/linux/tftpboot/ -f
+cp arch/arm/boot/dts/imx6ull-alientek-emmc.dtb /home/bo/linux/tftpboot/ -f
