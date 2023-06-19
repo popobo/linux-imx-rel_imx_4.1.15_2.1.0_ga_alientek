211d2d4cb161d9982d92f557696d365173b7cb91
popobo <331361996@qq.com>
2023-06-19 10:50:10 +0800

    add pinctrl_beep and gpiobeep to device tree

--- imx6ull_alientek_emmc.sh
+++ imx6ull_alientek_emmc.sh
@@ -3,5 +3,6 @@
 # make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf imx_alientek_emmc_defconfig
 # make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
 # make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j4
+make dtbs
 cp arch/arm/boot/zImage /home/bo/linux/tftpboot/ -f
 cp arch/arm/boot/dts/imx6ull-alientek-emmc.dtb /home/bo/linux/tftpboot/ -f
